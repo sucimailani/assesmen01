fb359ea6d94813f799232b7f7f825c458c1cef42
Suci Mailanisafitri <sucimailani20@gmail.com>
2025-04-10 02:29:21 +0700

    Menambahkan gambar serta membuat inputan user dan dropdownya

--- app/src/main/java/com/suci0008/laundry/MainActivity.kt
+++ app/src/main/java/com/suci0008/laundry/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.suci0008.laundry
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.suci0008.laundry.ui.theme.LaundryTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,12 +71,94 @@ fun MainScreen() {
     }
 }
 
+@SuppressLint("DefaultLocale")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    var nama by remember { mutableStateOf(TextFieldValue()) }
+    var layanan by remember { mutableStateOf("Reguler") }
+    var berat by remember { mutableStateOf(TextFieldValue()) }
+    var expanded by remember { mutableStateOf(false) }
+
+    val layananOptions = listOf("Reguler", "Express", "Kilat")
+    val hargaPerKg = when (layanan) {
+        "Reguler" -> 7000
+        "Express" -> 10000
+        "Kilat" -> 12000
+        else -> 0
+    }
+    val beratCucian = berat.text.toDoubleOrNull() ?: 0.0
+    val totalHarga = hargaPerKg * beratCucian
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.banner_laundry),
+            contentDescription = stringResource(R.string.banner),
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+        )
+        OutlinedTextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = { Text(stringResource(id = R.string.nama_pemesan))},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded},
+        ) {
+            OutlinedTextField(
+            value = layanan,
+            onValueChange = {},
+            readOnly = true,
+            label = { Text(stringResource(id = R.string.jenis_layanan))},
+            modifier = Modifier.menuAnchor().fillMaxWidth(),
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+                colors = ExposedDropdownMenuDefaults.textFieldColors()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                layananOptions.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option) },
+                        onClick = {
+                            layanan = option
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = berat,
+            onValueChange = { berat = it},
+            label = { Text("Berat Cucian (kg)")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Harga per kg: Rp$hargaPerKg",
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            text = "Total: Rp${String.format("%,.0f", totalHarga)}",
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+
 }
 
 
