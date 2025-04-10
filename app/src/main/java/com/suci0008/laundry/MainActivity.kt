007f1fed5de5c7be6d19281fb360f1bd1db098ec
Suci Mailanisafitri <sucimailani20@gmail.com>
2025-04-11 04:22:54 +0700

    Membuat navigasi bagikan

--- app/src/main/java/com/suci0008/laundry/MainActivity.kt
+++ app/src/main/java/com/suci0008/laundry/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.suci0008.laundry
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,15 +11,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -28,21 +33,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -94,13 +98,13 @@ fun MainScreen(navController: NavHostController) {
     }
 }
 
-@SuppressLint("DefaultLocale")
+@SuppressLint("DefaultLocale", "StringFormatMatches")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    var nama by rememberSaveable { mutableStateOf(TextFieldValue()) }
+    var nama by rememberSaveable { mutableStateOf("") }
     var layanan by rememberSaveable { mutableStateOf("Reguler") }
-    var berat by rememberSaveable { mutableStateOf(TextFieldValue()) }
+    var berat by rememberSaveable { mutableStateOf("") }
     var expanded by rememberSaveable { mutableStateOf(false) }
     var showDialog by rememberSaveable { mutableStateOf(false) }
 
@@ -111,8 +115,10 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         "Kilat" -> 12000
         else -> 0
     }
-    val beratCucian = berat.text.toDoubleOrNull() ?: 0.0
+    val beratCucian = berat.toDoubleOrNull() ?: 0.0
     val totalHarga = hargaPerKg * beratCucian
+    val context = LocalContext.current
+
     Column(
         modifier = modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.Top,
@@ -179,29 +185,63 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         }
 
         if (showDialog) {
-            AlertDialog(
-                onDismissRequest = {showDialog = false },
-                title = { Text(text = "Total Biaya Laundry")},
-                text = {
-                    Column {
-                        Text(text = "Nama: ${nama.text}")
-                        Text(text = "Layanan: $layanan")
-                        Text(text = "Berat: $beratCucian kg")
-                        Text(text = "Harga per kg: Rp$hargaPerKg")
-                        Text(text = "Total: Rp${String.format("%,.0f", totalHarga)}")
+            Card(
+                colors = CardDefaults.cardColors(containerColor = Color(0xFFFFF3E0)
+                ),
+                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                elevation = CardDefaults.cardElevation(2.dp),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(
+                        text = "Total Biaya Laundry Anda",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = Color(0xFFBF360C)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Nama: $nama")
+                    Text(text = "Layanan: $layanan")
+                    Text(text = "Berat: $beratCucian kg")
+                    Text(text = "Harga per kg: Rp${hargaPerKg}")
+                    Text(
+                        text = "Total: Rp${String.format("%,.0f", totalHarga)}",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.Black
+                    )
+                    Button(
+                        onClick = {
+                            shareData(context = context,
+                                message = context.getString(R.string.share_template,
+                                    nama, layanan, beratCucian, hargaPerKg, totalHarga)
+                            )
+                        },
+                        modifier = Modifier.padding(top = 6.dp),
+                        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 10.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.share),
+                            style = MaterialTheme.typography.bodySmall
+                        )
                     }
-                },
-                confirmButton = {
-                    TextButton(onClick = { showDialog = false}) {
-                        Text("OK")
-                    }
+
                 }
-            )
+            }
         }
     }
 }
 
-
+private fun shareData(context: Context, message: String) {
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, message)
+    }
+    if (shareIntent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(shareIntent)
+    }
+}
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
--- app/src/main/java/com/suci0008/laundry/screen/AboutScreen.kt
+++ app/src/main/java/com/suci0008/laundry/screen/AboutScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
